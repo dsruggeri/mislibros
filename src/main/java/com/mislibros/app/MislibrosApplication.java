5973f244f0ad54f4f7e464f6b50bc0a0b60cae21
diego <dsruggeri@gmail.com>
2022-08-25 17:34:38 -0300

    Primer commit.

---
+++ src/main/java/com/mislibros/app/MislibrosApplication.java
@@ -0,0 +1,13 @@
+package com.mislibros.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MislibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MislibrosApplication.class, args);
+	}
+
+}

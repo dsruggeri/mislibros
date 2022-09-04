47229e0b12792de39d9c70fc5c93aae766179230
diego <dsruggeri@gmail.com>
2022-09-04 12:04:05 -0300

    package services creados

--- src/main/java/com/mislibros/app/repositories/ContactoRepository.java
+++ src/main/java/com/mislibros/app/repositories/ContactoRepository.java
@@ -2,14 +2,20 @@
 package com.mislibros.app.repositories;
 
 import com.mislibros.app.models.Contacto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactoRepository extends JpaRepository<Contacto, String>{
     
-    // buscar por nombre y/o apellido
-    // buscar por whatsapp
+    // buscar por nombre y/o apellido y/o mail y/o whatsapp
+    @Query("SELECT c FROM Contacto c WHERE c.nombre OR contacto.apellido "
+            + " OR contacto.whatsapp OR contacto.email LIKE %:busqueda%")
+    public List<Contacto> buscarPorAutor(@Param("busqueda") String busqueda);
+
    
     
 }
